261bf04ec0d3ed3fffd24ba7f6e7c797eb284954
Mustafee <mussemoh03@gmail.com>
2026-04-09 10:18:36 +0200

    added role based folders

---
+++ MainApp/Pages/Seller/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MainApp.Pages.Seller
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
