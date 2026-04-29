f8a113edf93db5a2da780baf7a4fba4aaf7bf79e
Mustafee <mussemoh03@gmail.com>
2026-04-29 20:15:12 +0200

    added api

---
+++ AuctionApi/Program.cs
@@ -0,0 +1,35 @@
+using Data.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContexts>(options =>
+         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+                  options.SwaggerEndpoint("/openapi/v1.json", "Auction API"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => Results.Redirect("/swagger"))
+             .ExcludeFromDescription();
+
+app.Run();
